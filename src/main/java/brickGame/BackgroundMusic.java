fffcab99188e8412089f0eb9b67feceb81a5af23
Wong Yu Le <YuLeWong2002@gmail.com>
2023-12-08 14:39:27 +0800

    Added background music

---
+++ src/main/java/brickGame/BackgroundMusic.java
@@ -0,0 +1,48 @@
+package brickGame;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
+import java.io.File;
+
+public class BackgroundMusic {
+    private MediaPlayer mediaPlayer;
+
+    public BackgroundMusic(String mediaPath) {
+        // Convert the file path to a URI with the "file" scheme
+        File file = new File(mediaPath);
+        String uriString = file.toURI().toString();
+
+        Media media = new Media(uriString);
+        mediaPlayer = new MediaPlayer(media);
+    }
+
+    public void play() {
+        if (mediaPlayer != null) {
+            mediaPlayer.play();
+        }
+    }
+
+    public void pause() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    public void seek(Duration duration) {
+        if (mediaPlayer != null) {
+            mediaPlayer.seek(duration);
+        }
+    }
+
+    public boolean isPlaying() {
+        return mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING;
+    }
+}
