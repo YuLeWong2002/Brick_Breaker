a10ed3f044cf0ebe50e07b70c01ead96f1807a4e
Wong Yu Le <YuLeWong2002@gmail.com>
2023-12-10 22:02:40 +0800

    Delete redundant code

--- src/main/java/brickGame/Main.java
+++ src/main/java/brickGame/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
@@ -16,19 +15,14 @@ public class Main extends Application {
     private static BackgroundMusic backgroundMusic;
     public static String savePath    = "D:/save/save.mdds";
     String mediaPath = "src/main/resources/GameMusic.mp3";
-    Stage primaryStage;
     public static Scene scene;
     public static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        this.primaryStage = primaryStage;
         if(uiController == null) {
             uiController = new UIController(this, primaryStage);
         }
@@ -46,7 +40,6 @@ public class Main extends Application {
         }
         uiController.initializeUI();
     }
-
     public static void main(String[] args) { launch(args); }
     public static UIController getUiController() { return uiController; }
     public static GameInitializer getGameInitializer() { return gameInitializer; }
