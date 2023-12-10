da63bde1820f0e0dcd242c504a8c0d41f725c04a
Wong Yu Le <YuLeWong2002@gmail.com>
2023-12-10 22:09:37 +0800

    Add javadoc comments

--- src/main/java/brickGame/Main.java
+++ src/main/java/brickGame/Main.java
@@ -16,32 +16,94 @@ public class Main extends Application {
     public static String savePath    = "D:/save/save.mdds";
     String mediaPath = "src/main/resources/GameMusic.mp3";
     public static Scene scene;
+    /**
+     * Loads an FXML file and returns the root element of the corresponding scene graph.
+     *
+     * @param fxml The name of the FXML file (without the file extension) to be loaded.
+     * @return The root element of the scene graph loaded from the specified FXML file.
+     * @throws IOException If an I/O error occurs during the loading of the FXML file.
+     */
     public static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
+
+    /**
+     * Overrides the start method of the Application class.
+     * Initializes various components such as UIController, GameInitializer,
+     * MainMenuController, and BackgroundMusic. Starts background music if it is not playing,
+     * and initializes the UI through UIController.
+     *
+     * @param primaryStage The primary stage for the JavaFX application.
+     * @throws Exception If an exception occurs during the initialization or execution of the start method.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        if(uiController == null) {
+        // Initialize UIController if not already initialized
+        if (uiController == null) {
             uiController = new UIController(this, primaryStage);
         }
-        if(gameInitializer == null) {
+
+        // Initialize GameInitializer if not already initialized
+        if (gameInitializer == null) {
             gameInitializer = new GameInitializer(this);
         }
-        if(mainMenuController == null) {
+
+        // Initialize MainMenuController if not already initialized
+        if (mainMenuController == null) {
             mainMenuController = new MainMenuController(this);
         }
-        if(backgroundMusic == null) {
+
+        // Initialize BackgroundMusic if not already initialized
+        if (backgroundMusic == null) {
             backgroundMusic = new BackgroundMusic(mediaPath);
         }
-        if(!backgroundMusic.isPlaying()) {
+
+        // Start playing background music if it is not already playing
+        if (!backgroundMusic.isPlaying()) {
             backgroundMusic.play();
         }
+
+        // Initialize the UI through UIController
         uiController.initializeUI();
     }
-    public static void main(String[] args) { launch(args); }
-    public static UIController getUiController() { return uiController; }
-    public static GameInitializer getGameInitializer() { return gameInitializer; }
-    public static BackgroundMusic getBackgroundMusic() { return backgroundMusic; }
+
+    /**
+     * The main entry point for the JavaFX application.
+     *
+     * @param args Command-line arguments passed to the application (not used in this example).
+     */
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        launch(args);
+    }
+
+    /**
+     * Retrieves the UIController instance associated with the application.
+     *
+     * @return The UIController instance.
+     */
+    public static UIController getUiController() {
+        return uiController;
+    }
+
+    /**
+     * Retrieves the GameInitializer instance associated with the application.
+     *
+     * @return The GameInitializer instance.
+     */
+    public static GameInitializer getGameInitializer() {
+        return gameInitializer;
+    }
+
+    /**
+     * Retrieves the BackgroundMusic instance associated with the application.
+     *
+     * @return The BackgroundMusic instance.
+     */
+    public static BackgroundMusic getBackgroundMusic() {
+        return backgroundMusic;
+    }
+
 }
