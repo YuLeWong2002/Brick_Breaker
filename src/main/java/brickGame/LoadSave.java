c22d584cfcb1b66a2143ba2534784d436021da92
Wong Yu Le <YuLeWong2002@gmail.com>
2023-12-11 02:21:46 +0800

    Added javadoc comments

--- src/main/java/brickGame/LoadSave.java
+++ src/main/java/brickGame/LoadSave.java
@@ -6,34 +6,139 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
+/**
+ * Represents the state of the game, including various flags, scores, and positions.
+ */
 public class LoadSave {
-    public boolean          isExistHeartBlock;
-    public boolean          isGoldStatus;
-    public boolean          goDownBall;
-    public boolean          goRightBall;
-    public boolean          collideToBreak;
-    public boolean          collideToBreakAndMoveToRight;
-    public boolean          collideToRightWall;
-    public boolean          collideToLeftWall;
-    public boolean          collideToRightBlock;
-    public boolean          collideToBottomBlock;
-    public boolean          collideToLeftBlock;
-    public boolean          collideToTopBlock;
-    public int              level;
-    public int              score;
-    public int              heart;
-    public int              destroyedBlockCount;
-    public double           xBall;
-    public double           yBall;
-    public double           xBreak;
-    public double           yBreak;
-    public double           centerBreakX;
-    public long             time;
-    public long             goldTime;
-    public double           vX;
+    /**
+     * Indicates whether a heart block exists in the game.
+     */
+    public boolean isExistHeartBlock;
+
+    /**
+     * Indicates whether the gold status is active.
+     */
+    public boolean isGoldStatus;
+
+    /**
+     * Indicates whether the ball should move down.
+     */
+    public boolean goDownBall;
+
+    /**
+     * Indicates whether the ball should move to the right.
+     */
+    public boolean goRightBall;
+
+    /**
+     * Indicates whether the ball should collide to paddle.
+     */
+    public boolean collideToBreak;
+
+    /**
+     * Indicates whether the ball should collide to paddle and move to the right.
+     */
+    public boolean collideToBreakAndMoveToRight;
+
+    /**
+     * Indicates whether the ball has collided with the right wall.
+     */
+    public boolean collideToRightWall;
+
+    /**
+     * Indicates whether the ball has collided with the left wall.
+     */
+    public boolean collideToLeftWall;
+
+    /**
+     * Indicates whether the ball has collided with a block on the right.
+     */
+    public boolean collideToRightBlock;
+
+    /**
+     * Indicates whether the ball has collided with a block on the bottom.
+     */
+    public boolean collideToBottomBlock;
+
+    /**
+     * Indicates whether the ball has collided with a block on the left.
+     */
+    public boolean collideToLeftBlock;
+
+    /**
+     * Indicates whether the ball has collided with a block on the top.
+     */
+    public boolean collideToTopBlock;
+
+    /**
+     * Represents the current level of the game.
+     */
+    public int level;
+
+    /**
+     * Represents the current score in the game.
+     */
+    public int score;
+
+    /**
+     * Represents the number of hearts in the game.
+     */
+    public int heart;
+
+    /**
+     * Represents the count of destroyed blocks in the game.
+     */
+    public int destroyedBlockCount;
+
+    /**
+     * The x-coordinate of the ball's position.
+     */
+    public double xBall;
+
+    /**
+     * The y-coordinate of the ball's position.
+     */
+    public double yBall;
+
+    /**
+     * The x-coordinate of the paddle position.
+     */
+    public double xBreak;
+
+    /**
+     * The y-coordinate of the paddle position.
+     */
+    public double yBreak;
+
+    /**
+     * The center x-coordinate of the break position.
+     */
+    public double centerBreakX;
+
+    /**
+     * The current time in the game.
+     */
+    public long time;
+
+    /**
+     * The time when the gold status is activated.
+     */
+    public long goldTime;
+
+    /**
+     * The velocity of the ball.
+     */
+    public double vX;
+
+    /**
+     * List of blocks in the game.
+     */
     public ArrayList<BlockSerializable> blocks = new ArrayList<BlockSerializable>();
 
-
+    /**
+     * Reads the game state from a saved file using ObjectInputStream.
+     * The method populates the instance fields of the LoadSave object with the saved values.
+     */
     public void read() {
 
 
