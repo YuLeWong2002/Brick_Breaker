257950fb506c11b0fde4a9d7336c1a0728d8e63d
Wong Yu Le <YuLeWong2002@gmail.com>
2023-12-11 01:38:40 +0800

    Added javadoc comments

--- src/main/java/brickGame/BackgroundMusic.java
+++ src/main/java/brickGame/BackgroundMusic.java
@@ -6,9 +6,23 @@ import javafx.util.Duration;
 
 import java.io.File;
 
+/**
+ * The {@code BackgroundMusic} class represents a utility class for managing background music in a JavaFX application.
+ * It utilizes the {@link javafx.scene.media.MediaPlayer} class to play, pause, resume, stop, and check the playing
+ * status of background music.
+ */
 public class BackgroundMusic {
-    private MediaPlayer mediaPlayer;
 
+    /**
+     * Media player for controlling background music.
+     */
+    private final MediaPlayer mediaPlayer;
+
+    /**
+     * Constructs a new BackgroundMusic instance.
+     *
+     * @param mediaPath The file path to the media resource.
+     */
     public BackgroundMusic(String mediaPath) {
         // Convert the file path to a URI with the "file" scheme
         File file = new File(mediaPath);
@@ -20,36 +34,47 @@ public class BackgroundMusic {
         mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
     }
 
+    /**
+     * Starts playing the background music.
+     */
     public void play() {
         if (mediaPlayer != null) {
             mediaPlayer.play();
         }
     }
 
+    /**
+     * Pauses the background music.
+     */
     public void pause() {
         if (mediaPlayer != null) {
             mediaPlayer.pause();
         }
     }
 
+    /**
+     * Resumes the background music if it was previously paused.
+     */
     public void resume() {
         if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PAUSED) {
             mediaPlayer.play();
         }
     }
 
+    /**
+     * Stops the background music.
+     */
     public void stop() {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
     }
 
-    public void seek(Duration duration) {
-        if (mediaPlayer != null) {
-            mediaPlayer.seek(duration);
-        }
-    }
-
+    /**
+     * Checks if the background music is currently playing.
+     *
+     * @return {@code true} if the background music is playing, {@code false} otherwise.
+     */
     public boolean isPlaying() {
         return mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING;
     }
