5d7e78fa80c61a283d9e17a3631d112243081f88
Wong Yu Le <YuLeWong2002@gmail.com>
2023-12-11 02:04:10 +0800

    Added javadoc comments

--- src/main/java/brickGame/BlockSerializable.java
+++ src/main/java/brickGame/BlockSerializable.java
@@ -2,12 +2,27 @@ package brickGame;
 
 import java.io.Serializable;
 
+/**
+ * A serializable representation of a Block.
+ */
 public class BlockSerializable implements Serializable {
+    /** The row of the Block. */
     public final int row;
+
+    /** The column of the Block. */
     public final int j;
+
+    /** The type of the Block. */
     public final int type;
 
-    public BlockSerializable(int row , int j , int type) {
+    /**
+     * Constructs a BlockSerializable with the specified row, column, and type.
+     *
+     * @param row  The row of the Block.
+     * @param j    The column of the Block.
+     * @param type The type of the Block.
+     */
+    public BlockSerializable(int row, int j, int type) {
         this.row = row;
         this.j = j;
         this.type = type;
