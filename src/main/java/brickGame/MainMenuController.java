6d0306b4631366fa63e09ba9e115a46a115a29df
Wong Yu Le <YuLeWong2002@gmail.com>
2023-12-11 00:59:19 +0800

    Added javadoc comments

--- src/main/java/brickGame/MainMenuController.java
+++ src/main/java/brickGame/MainMenuController.java
@@ -5,38 +5,49 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import java.io.IOException;
 
-
+/**
+ * Controller class for the main menu screen. Handles user interactions and initiates game-related actions.
+ */
 public class MainMenuController {
 
+    /** Button for loading a saved game. */
     @FXML
     private Button load;
+
+    /** Button for starting a new game. */
     @FXML
     private Button newGame;
-    private Main main;
 
+    /**
+     * Default constructor for the MainMenuController class.
+     * This constructor is automatically called when the FXML file is loaded.
+     */
     public MainMenuController() {
     }
 
-    public MainMenuController(Main main) {
-        this.main = main;
-    }
-
+    /**
+     * Event handler method for the "Load" button click.
+     * Switches to the game scene, initializes game elements, and loads a saved game.
+     *
+     * @throws IOException If an error occurs during the loading of the game.
+     */
     @FXML
     private void onClickLoad() throws IOException {
         Main.getUiController().switchToGameScene();
         GameController gameController = Main.getUiController().getGameController();
         Main.getGameInitializer().initBall(gameController.getRoot());
         Main.getGameInitializer().initBreak(gameController.getRoot());
-//        GameController gameController = Main.getUiController().getGameController();
         gameController.getGameIOController().loadGame();
-//        Main.getUiController().switchToGameScene();
-//        Main.getUiController().startLoadGameElements();
-//        GameController gameController = Main.getUiController().getGameController();
-//        gameController.getGameIOController().loadGame();
         load.setVisible(false);
         newGame.setVisible(false);
     }
 
+    /**
+     * Event handler method for the "New Game" button click.
+     * Switches to the game scene and starts a new game with initialized elements.
+     *
+     * @throws IOException If an error occurs during the initialization of the new game.
+     */
     @FXML
     private void onClickNewGame() throws IOException{
         load.setVisible(false);
